603b3d3db1de0f48fcbdcace2cbe7546702297d3
Thiago Henrique <tiagohsilva15@outlook.com.br>
2025-04-18 16:19:27 -0300

    Concluido Aulas do 5 ao 7

---
+++ Python/Aula_5.py
@@ -0,0 +1,39 @@
+preco = 10
+print(preco)
+
+preco = float(preco)
+print(preco)
+
+preco = 10 / 2
+print(preco)
+
+preco = 10.30
+print(preco)
+
+preco = int(preco)
+print(preco)
+
+preco = 10
+print(preco)
+
+print(preco / 2)
+
+print(preco // 2) #Divisao com duas barras mantem o numero inteiro
+
+preco = 10.5
+idade = 28
+
+print(str(preco))
+print(str(idade))
+
+texto = f"idade {idade} preco {preco}" #Concatena string com variaveis
+print(texto)
+
+preco = "10.50"
+idade = "28"
+
+print(float(preco))
+print(int(idade))
+
+preco = "python"    
+print(float(preco))
