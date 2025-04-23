425935f063c00cee38016e3035ee25fa08ade2a3
Thiago Henrique <tiagohsilva15@outlook.com.br>
2025-04-23 08:39:22 -0300

    Concluido Aula 4

---
+++ Python/Aula_4.py
@@ -0,0 +1,14 @@
+nome = "Thiago"
+idade = 24
+
+nome = "Henrique"
+
+nome, idade = "Leticia", 23
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZIILIAN_SATATES = ["SP", "RJ", "SC", "RS"]
+
+print(BRAZIILIAN_SATATES)
